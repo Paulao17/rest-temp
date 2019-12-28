b70417eb2c413833e14a168072001005c8ecef12
Paulao17 <paul@shimaore.net>
2019-12-28 18:44:22 +0100

    Apidoc correction Removed the apiDefine of username and password

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -5,19 +5,12 @@ const passport = require('../../services/passport')
 const router = new Router()
 
 /**
- * @apiDefine username
- * @apiParam {String{2..32}} username The username of the user.
- */
-/**
- * @apiDefine password
- * @apiParam {String{6..128}} password The password of the user.
- */
-/**
  * @api {post} /auth Authenticate
+ * @apiVersion 0.0.1
  * @apiName Authenticate
  * @apiGroup Auth
- * @apiUse username
- * @apiUse password
+ * @apiParam {String{2..32}} username The username of the user.
+ * @apiParam {String{6..128}} password The password of the user.
  * @apiSuccess (Success 201) {String} token JWT to be passed to other requests.
  * @apiError 401 Invalid credentials.
  * @apiExample {curl} Example usage:
